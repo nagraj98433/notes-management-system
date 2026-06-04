535c483bca9dd7d06a07dd08f469bcf32750fa11
Raj Vade <rajkapool123@gmail.com>
2026-06-04 08:06:10 +0530

    Add deployment configuration

--- frontend/src/services/noteService.js
+++ frontend/src/services/noteService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = "http://localhost:5000/api/notes";
+const API = "https://notes-management-system-rzyz.onrender.com/api/notes";
 
 export const getNotes = () => axios.get(API);
 
